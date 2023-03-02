0c3b257d5c4b1c9b6b655de495781a80b371c6bd
Renars Ozols <renars.ozols@magebit.com>
2023-03-02 11:00:45 +0200

    implemented theme task

---
+++ app/design/frontend/Magebit/learning/requirejs-config.js
@@ -0,0 +1,23 @@
+/**
+ * This file is part of the Magebit theme package.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magebit_AmastyFaq
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2023 Magebit, Ltd. (https://magebit.com/)
+ * @author    Magebit <info@magebit.com>
+ * @license   MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+var config = {
+    map: {
+        '*': {
+            'qty-counter': 'Magento_Catalog/js/qty-counter'
+        }
+    }
+};
